f6af3a11fc651d38d8508494f2333c7924f7b6da
Daniel Fridljand <daniel.fridljand@gmail.com>
2020-11-17 17:02:10 +0100

    optimised index search in exposure

--- 01_get_exp.R
+++ 01_get_exp.R
@@ -9,7 +9,7 @@
 
 # load packages, install if missing
 
-packages <- c("RCurl")
+packages <- c("RCurl","magrittr")
 
 for(p in packages){
   if(p %in% rownames(installed.packages())==FALSE){
@@ -45,6 +45,14 @@ if (!file.exists(expDir)){
   close(fileConn)
 } 
 
+tmpDir <-file.path(dataDir, "tmp")
+if (!file.exists(expDir)){
+  dir.create(tmpDir)
+  fileConn<-file(file.path(expDir,"readme.txt"))
+  writeLines(c("This directory contains variables used in calculations"), fileConn)
+  close(fileConn)
+} 
+
 #------------------Functions--------------------------------------------------
 #download 
 
@@ -58,6 +66,25 @@ downloadYear <-function(year){
   print(paste("Downloading PM exposure data for",year))
   download.file(paste(url, filename, sep = ""), filepath)
   print(paste("Successfully downloaded PM exposure data for",year))
+  
+  #save useful variable #TODO
+  exp_data <- H5Fopen(filepath)
+  
+  long_vec <-  c(as.matrix(exp_data$longitude)) 
+  lat_vec <- c(as.matrix(exp_data$latitude))
+  
+  n_long<-length(long_vec)-1
+  long_dif<-long_vec[2:(1+n_long)]-long_vec[1:n_long]
+  m_min_long<-min(long_dif) 
+  m_max_long<-max(long_dif)
+  
+  n_lat<-length(lat_vec)-1
+  lat_dif<-lat_vec[2:(1+n_lat)]-lat_vec[1:n_lat]
+  m_min_lat<-min(lat_dif) 
+  m_max_lat<-max(lat_dif)
+  
+  #save(m_min_long, m_max_long, m_min_lat, m_max_lat, file = "data.RData")
+  #TODO save
 }
 
 #getter functions
@@ -76,16 +103,58 @@ getExposureH5F <- function(year){
   if (!file.exists(filepath)){
     downloadYear(year)
   }
-  exp_data = H5Fopen(filepath)
+  exp_data <- H5Fopen(filepath)
   return(exp_data)
   #TODO return
 }
 
+
 getExposure <- function(year, long, lat){
+  
   exp_data <- getExposureH5F(year)
-  #TODO
+  
+  long_vec <-  c(as.matrix(exp_data$longitude)) #TODO more efficient
+  lat_vec <- c(as.matrix(exp_data$latitude))
+  
+  #make sure in grid
+  n<-length(long_vec)-1
+  a<-long_vec[2:(1+n)]-long_vec[1:n]
+  m_min_long<-min(a) #TODO save
+  m_max_long<-max(a)
+  
+  #test todo delete
+  year<-2016
+  #ru <-runif(1, min=-140,max=-120)
+  #for(long in ru){
+  long<-120.5056
+  long_clos <- sum(long,0.005)%>%
+                  round(.,digit=2) %>%
+                  sum(-0.005) %>%
+                  min(.,long_vec[length(long_vec)]) %>%
+                  max(.,long_vec[1]) 
+  r1<-floor(1+(long_clos-long_vec[1])/m_max_long)
+  r2<-ceiling(1+(long_clos-long_vec[1])/m_min_long)
+  subset<-long_vec[r1:r2]
+  r_final <- r1+which.min(abs(long_vec[r1:r2]-long_clos))-1 #TODO pipe
+  r_final2 <- long_vec[r1:r2]-long_clos #%>%
+                  #abs %>%
+                  #which.min %>%
+                  #sum(r1-1)
+
+  #}
+  
+  lat_clos <- min(lat,lat_vec[length(lat_vec)]) %>%
+                  max(lat_vec[1]) %>%
+                  round(digit=2) %>%
+                  sum(-0.005)
+  
+  
+  #findInterval
+  #binsearch
+  #https://stackoverflow.com/questions/20133344/find-closest-value-in-a-vector-with-binary-search
+  #http://adomingues.github.io/2015/09/24/finding-closest-element-to-a-number-in-a-list/
+  #TODO divide by 100
 }
 
 #------------------run tests--------------------------------------------------
-year<-2016
-getExposureH5F(year)
+
