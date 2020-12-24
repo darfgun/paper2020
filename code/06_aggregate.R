144207a9675889e357d96ffe1eaf0f405ffed242
darfgun <daniel.fridljand@gmail.com>
2020-12-24 20:12:43 +0100

    comment

--- code/06_aggregate.R
+++ code/06_aggregate.R
@@ -28,18 +28,18 @@ cens_agrDir <- args[9]
 agr_by <- args[10]
 
 # TODO l?schen
-year <- 2016
-agr_by <- "county"
+#year <- 2016
+#agr_by <- "county"
 
 # tmpDir <- "/Users/default/Desktop/own_code2/data/tmp"
 # exp_tracDir <- "/Users/default/Desktop/own_code2/data/03_exp_tracts"
 # censDir <- "/Users/default/Desktop/own_code2/data/06_demog"
 # cens_agrDir <- "/Users/default/Desktop/own_code2/data/07_dem.agr"
 
-tmpDir <- "C:/Users/Daniel/Desktop/paper2020/data/tmp"
-exp_tracDir <- "C:/Users/Daniel/Desktop/paper2020/data/03_exp_tracts"
-censDir <- "C:/Users/Daniel/Desktop/paper2020/data/06_demog"
-cens_agrDir <- "C:/Users/Daniel/Desktop/paper2020/data/07_dem.agr"
+#tmpDir <- "C:/Users/Daniel/Desktop/paper2020/data/tmp"
+#exp_tracDir <- "C:/Users/Daniel/Desktop/paper2020/data/03_exp_tracts"
+#censDir <- "C:/Users/Daniel/Desktop/paper2020/data/06_demog"
+#cens_agrDir <- "C:/Users/Daniel/Desktop/paper2020/data/07_dem.agr"
 
 if (!agr_by %in% c("county", "Census_Region", "Census_division", "hhs_region_number", "state", "nation")) {
   print(paste(agr_by, "is an invalid agr_by argument"))
